8f8c1699ee930871a779cc0c491d6dec0e5cc53e
carowann <cwannhed@student.42firenze.it>
2025-10-15 12:00:21 +0200

    .gitignore

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,18 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
+/*   By: cwannhed <cwannhed@student.42firenze.it>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:52:05 by cwannhed          #+#    #+#             */
-/*   Updated: 2025/08/07 15:28:42 by cwannhed         ###   ########.fr       */
+/*   Updated: 2025/10/15 11:53:27 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-//ideas for solution
-//data struct w/ priority, whoever ate too long ago, jumps the queue
-//mutual exclusion: no process can access shared data and cause data races
 
 int	main(int argc, char *argv[])
 {
